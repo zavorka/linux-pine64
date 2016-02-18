687e913dea69c405333788549817060d5f8c97b9
Simon Eisenmann <simon@longsleep.org>
2016-02-18 21:09:12 +0100

    Hacked mmc shutdown to not crash with BUG, fixing reboot.

--- drivers/mmc/host/sunxi-mmc.c
+++ drivers/mmc/host/sunxi-mmc.c
@@ -1145,6 +1145,92 @@ static void sunxi_mmc_request(struct mmc_host *mmc, struct mmc_request *mrq)
 	spin_unlock_irqrestore(&host->lock, iflags);
 }
 
+extern int mmc_go_idle(struct mmc_host *host);
+extern int mmc_send_op_cond(struct mmc_host *host, u32 ocr, u32 *rocr);
+extern int mmc_send_status(struct mmc_card *card, u32 *status);
+extern void mmc_set_clock(struct mmc_host *host, unsigned int hz);
+extern void mmc_set_timing(struct mmc_host *host, unsigned int timing);
+extern void mmc_set_bus_width(struct mmc_host *host, unsigned int width);
+void sunxi_mmc_do_shutdown(struct platform_device * pdev)
+{
+	u32 ocr = 0;
+	u32 err = 0;
+	struct mmc_host *mmc = NULL;
+	struct sunxi_mmc_host *host = NULL;
+	u32 status = 0;
+
+	mmc = platform_get_drvdata(pdev);
+	if (mmc == NULL) {
+		dev_err(&pdev->dev,"%s: mmc is NULL\n", __FUNCTION__);
+		goto out;
+	}
+
+	host = mmc_priv(mmc);
+	if (host == NULL) {
+		dev_err(&pdev->dev,"%s: host is NULL\n", __FUNCTION__);
+		goto out;
+	}
+
+	dev_info(mmc_dev(mmc),"try to disable cache\n");
+	mmc_claim_host(mmc);
+    err = mmc_cache_ctrl(mmc, 0);
+	mmc_release_host(mmc);
+    if (err){
+		dev_err(mmc_dev(mmc),"disable cache failed\n");
+		mmc_claim_host(mmc);//not release host to not allow android to read/write after shutdown
+         goto out;
+    }
+
+	// XXX(longsleep): mmc_send_status below triggers BUG, do nothing and pray!
+	goto out;
+
+	//claim host to not allow androd read/write during shutdown
+	dev_dbg(mmc_dev(mmc),"%s: claim host\n", __FUNCTION__);
+	mmc_claim_host(mmc);
+
+	do {
+		if (mmc_send_status(mmc->card, &status) != 0) {
+			dev_err(mmc_dev(mmc),"%s: send status failed\n", __FUNCTION__);
+			goto out; //err_out; //not release host to not allow android to read/write after shutdown
+		}
+	} while(status != 0x00000900);
+
+	//mmc_card_set_ddr_mode(card);
+	mmc_set_timing(mmc, MMC_TIMING_LEGACY);
+	mmc_set_bus_width(mmc, MMC_BUS_WIDTH_1);
+	mmc_set_clock(mmc, 400000);
+	err = mmc_go_idle(mmc);
+	if (err) {
+		dev_err(mmc_dev(mmc),"%s: mmc_go_idle err\n", __FUNCTION__);
+		goto out; //err_out; //not release host to not allow android to read/write after shutdown
+	}
+
+	if (mmc->card->type != MMC_TYPE_MMC) {//sd can support cmd1,so not send cmd1
+		goto out;//not release host to not allow android to read/write after shutdown
+	}
+
+	err = mmc_send_op_cond(mmc, 0, &ocr);
+	if (err) {
+		dev_err(mmc_dev(mmc),"%s: first mmc_send_op_cond err\n", __FUNCTION__);
+		goto out; //err_out; //not release host to not allow android to read/write after shutdown
+	}
+
+	err = mmc_send_op_cond(mmc, ocr | (1 << 30), &ocr);
+	if (err) {
+		dev_err(mmc_dev(mmc),"%s: mmc_send_op_cond err\n", __FUNCTION__);
+		goto out; //err_out; //not release host to not allow android to read/write after shutdown
+	}
+
+	//do not release host to not allow android to read/write after shutdown
+	goto out;
+
+out:
+	dev_info(mmc_dev(mmc),"%s: mmc shutdown exit..ok\n", __FUNCTION__);
+
+	return ;
+}
+
+
 /*we use our own mmc_regulator_get_supply because our platform regulator not support supply name,*/
 /*only support regulator ID,but linux mmc' own mmc_regulator_get_supply use supply name*/
 static int sunxi_mmc_regulator_get_supply(struct mmc_host *mmc)
