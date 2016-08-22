eed8e03928879409cd665801942e1f2ca2e88137
Kamil Trzcinski <ayufan@ayufan.eu>
2016-08-22 20:47:39 +0200

    Use global hdmi_base_addr

--- drivers/video/sunxi/disp2/hdmi/drv_hdmi.c
+++ drivers/video/sunxi/disp2/hdmi/drv_hdmi.c
@@ -37,6 +37,7 @@ static struct cdev *my_cdev;
 static dev_t devid ;
 static struct class *hdmi_class;
 hdmi_info_t ghdmi;
+
 static BLOCKING_NOTIFIER_HEAD(sunxi_hdmi_notifier_list);
 
 int register_sunxi_hdmi_notifier(struct notifier_block *nb)
